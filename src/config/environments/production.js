2f3a7934866aa88e648047bd5e44404f69472add
Bhautik <bhautik211298@gmail.com>
2023-02-24 17:17:13 +0530

    change port

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: "localhost",
-    port: 8000,
+    port: 8001,
     // mongoUrl: "mongodb://localhost:27017/Graphic",
     mongoUrl: "mongodb+srv://admin:admin@priycluster.qmk8ebl.mongodb.net/priyGraphic?retryWrites=true&w=majority",
     logLevel: "debug",
